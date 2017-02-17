d69ceded30e8ce42e329c47007260a35be8f1881
DESKTOP-NMEDHM2\Icons4u TI <eduardo@icons4u.com.br>
2017-02-17 17:05:32 -0200

    Tela login e form login

--- Vadiando/app/src/main/java/br/com/suic/vadiando/Login.java
+++ Vadiando/app/src/main/java/br/com/suic/vadiando/Login.java
@@ -1,7 +1,12 @@
 package br.com.suic.vadiando;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final Context contexto = this;
+
+        Button entrar = (Button)(findViewById(R.id.entrar));
+        entrar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(contexto,FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

fc40631d8127c3ec3a56055bfb1eab506b7fb143
DESKTOP-NMEDHM2\Icons4u TI <eduardo@icons4u.com.br>
2017-02-24 16:11:27 -0300

    Ajuste do botão

--- Vadiando/app/src/main/java/br/com/suic/vadiando/FormularioActivity.java
+++ Vadiando/app/src/main/java/br/com/suic/vadiando/FormularioActivity.java
@@ -1,7 +1,11 @@
 package br.com.suic.vadiando;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        final Context contexto = this;
+
+        Button formularioEnviar =(Button) findViewById(R.id.formularioEnviar);
+        formularioEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(contexto,RodasActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ Vadiando/app/src/main/java/br/com/suic/vadiando/ListaDeRodasActivity.java
@@ -0,0 +1,15 @@
+package br.com.suic.vadiando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaDeRodasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_rodas);
+
+
+    }
+}

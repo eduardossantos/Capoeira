a78bb699cf6602322a648fde01009f68dfcae985
DESKTOP-NMEDHM2\Icons4u TI <eduardo@icons4u.com.br>
2017-03-07 17:51:41 -0300

    listar rodas inicio

--- Vadiando/app/src/main/java/br/com/suic/vadiando/ListaDeRodasActivity.java
+++ Vadiando/app/src/main/java/br/com/suic/vadiando/ListaDeRodasActivity.java
@@ -3,12 +3,19 @@ package br.com.suic.vadiando;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import br.com.suic.vadiando.dao.UsuarioDAO;
+import br.com.suic.vadiando.models.Roda;
+
 public class ListaDeRodasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_rodas);
+        UsuarioDAO dao = new UsuarioDAO(this);
+        List<Roda> listaDeRodas =  dao.buscaRodas();
 
 
     }

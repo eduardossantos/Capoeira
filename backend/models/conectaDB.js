0b8336f69de099ab8146614228cf0e48f2a1d08d
Luiz Santos <luizsantos1992@gmail.com>
2017-04-26 15:08:39 -0300

    ajustando Conexao banco servidor

--- backend/models/conectaDB.js
+++ backend/models/conectaDB.js
@@ -2,17 +2,17 @@ exports.conectaDB = function(){
 	var Promise = require('bluebird');
 	var mysql      = require('mysql');
 	var connection = mysql.createConnection({
-	  host     : 'localhost',
+	  host     : '104.131.181.32',
 	  user     : 'root',
-	  password : '',
-	  database : 'capoeira'
+	  password : 'novaera2017',
+	  database : 'Capoeira'
 	});
 	
 	return new Promise(function(data, error){
 		connection.connect(function(err) {  		
 		  
 		  if (err) {
-		    error('erro :' + err.stack);
+		    error('erro :' + err);
 		    return;
 		  }
 		 
